0fef854b6873e5cb33c874c05da1084c614a749f
alexey.vasin <alexeyvasin@gmail.com>
2024-01-24 16:36:08 +0300

    added another 2  switches

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
-for _ in range(1000):
-    if 5==4 and 2/0:
-        print("yes")
-    else:
-        print ("no")
+message_1 = input('Введите первое сообщение: ')
+message_2 = input('Введите второе сообщение: ')
+
+l_message_1 = list(message_1)
+l_message_2 = list(message_2)
+
+count_1 = l_message_1.count('!') + l_message_1.count('?')
+count_2 = l_message_2.count('!') + l_message_2.count('?')
+
+if count_1 > count_2:
+    print(message_1, message_2)
+elif count_2 > count_1:
+    print(message_2, message_1)
+else:
+    print('Ой')
