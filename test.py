68b76a79516cb4fea41e541e9c224ef7688e7b5c
alexey.vasin <alexeyvasin@gmail.com>
2024-02-05 13:44:24 +0300

    done something

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
-message_1 = input('Введите первое сообщение: ')
-message_2 = input('Введите второе сообщение: ')
 
-l_message_1 = list(message_1)
-l_message_2 = list(message_2)
-
-count_1 = l_message_1.count('!') + l_message_1.count('?')
-count_2 = l_message_2.count('!') + l_message_2.count('?')
-
-if count_1 > count_2:
-    print(message_1, message_2)
-elif count_2 > count_1:
-    print(message_2, message_1)
-else:
-    print('Ой')
+fhand = open("getlic.txt", "r")
+ftext = fhand.read()
+crypttext=''
+for chr in ftext:
+	crypttext += str(ord(chr))
+	crypttext +=str(ord('!'))
+	crypttext +=str(ord('*'))
+	crypttext +=str(ord('&'))
+#message(ord('!'))
+#message(ord('*'))
+#message(ord('&'))
+fhand.close()
+fhand = open("Rename.lic", "w")
+fhand.write(crypttext)
+fhand.close()
