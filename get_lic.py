e05faf2a8c89f29fd44ce4dd56f70c627925e678
alexey.vasin <alexeyvasin@gmail.com>
2024-04-09 14:16:55 +0300

    done something

---
+++ get_lic.py
@@ -0,0 +1,15 @@
+fhand = open("getlic.txt", "r")
+ftext = fhand.read()
+crypttext=''
+for chr in ftext:
+	crypttext += str(ord(chr))
+	crypttext +=str(ord('!'))
+	crypttext +=str(ord('*'))
+	crypttext +=str(ord('&'))
+#message(ord('!'))
+#message(ord('*'))
+#message(ord('&'))
+fhand.close()
+fhand = open("Rename.lic", "w")
+fhand.write(crypttext)
+fhand.close()
